1bb953764878bf44cf74abc50b2300482bd93ef1
Slava <dyuminslava155@gmail.com>
2023-07-25 20:03:31 +0300

    ver 1.4 les7 add models

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField('Заголовок', max_length=128, )
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отметьте, уместен ли торг')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+# https://docs.djangoproject.com/en/4.2/ref/models/fields/
+# https://django.fun/ru/docs/django/4.1/ref/models/fields/
